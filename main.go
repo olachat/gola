ae86efaa861b9df9b32d7158a33775653629aaac
问天 Weng Wei <weng.wei@olaola.chat>
2022-01-15 21:40:51 +0800

    Code cleanup

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/olachat/gola/corelib"
@@ -17,50 +16,17 @@ type User struct {
 	user.Created
 }
 
-var types = make(map[reflect.Type]bool)
-
-func Print[T any, PT corelib.PointerType[T]](s PT) PT {
-	t := reflect.TypeOf(s)
-	flag := types[t]
-
-	s1 := new(T)
-	fmt.Printf("%v \n", s1)
-
-	fmt.Printf("%v %v %v\n", s, t, flag)
-	types[t] = true
-	return s1
-}
-
 type SimpleUser struct {
 	user.Name
 	user.Email
 }
 
+func Print[T any, PT corelib.PointerType[T]](s PT) {
+	fmt.Printf("%v\n", s)
+}
+
 func main() {
-	t := &User{}
-	t.Id = 1
-	Print(t)
-	t.SetName("piggy")
-	t2 := Print(t)
-	t2.SetName("bar")
-	fmt.Printf("%v\n", t2)
-	fmt.Printf("%v\n", t)
-
-	for i := 0; i < 5; i++ {
-		var q *struct {
-			user.Id
-		}
-		Print(q)
-	}
-	for i := 0; i < 5; i++ {
-		var q = new(struct {
-			user.Id
-		})
-		q.Id = 1
-		Print(user.Run())
-	}
-	println(t.GetName())
-
+	println("ExecScalar:")
 	u := corelib.ExecScalar[struct {
 		user.Email
 	}]()
