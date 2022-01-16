63d499eed297873f9d1143c5a52b2f53c81ab08e
问天 Weng Wei <weng.wei@olaola.chat>
2022-01-16 21:41:32 +0800

    Add IndexDesc

--- mysqldriver/index_key.go
+++ mysqldriver/index_key.go
@@ -21,6 +21,21 @@ type RowDesc struct {
 	Field, Type, Null, Key, Default, Extra string
 }
 
+/*
+show index from users;
++-------+------------+----------+--------------+-------------+-----------+-------------+----------+--------+------+------------+---------+---------------+---------+------------+
+| Table | Non_unique | Key_name | Seq_in_index | Column_name | Collation | Cardinality | Sub_part | Packed | Null | Index_type | Comment | Index_comment | Visible | Expression |
++-------+------------+----------+--------------+-------------+-----------+-------------+----------+--------+------+------------+---------+---------------+---------+------------+
+| users |          0 | email    |            1 | email       | NULL      |           0 |     NULL | NULL   |      | BTREE      |         |               | YES     | NULL       |
+| users |          1 | name     |            1 | name        | NULL      |           0 |     NULL | NULL   |      | BTREE      |         |               | YES     | NULL       |
++-------+------------+----------+--------------+-------------+-----------+-------------+----------+--------+------+------------+---------+---------------+---------+------------+
+*/
+
+type IndexDesc struct {
+	Table, Key_name, Column_name, Collation, Sub_part, Packed, Null, Index_type, Comment, Index_comment, Visible, Expression string
+	Non_unique, Seq_in_index, Cardinality                                                                                    int
+}
+
 func (m *MySQLDriver) SetIndexAndKey(dbinfo *drivers.DBInfo) (err error) {
 	for i := range dbinfo.Tables {
 		t := &dbinfo.Tables[i]
@@ -42,6 +57,18 @@ func (m *MySQLDriver) SetIndexAndKey(dbinfo *drivers.DBInfo) (err error) {
 				t.PKey.Name = rd.Field
 				t.PKey.Columns = []string{rd.Field}
 			}
+		}
+
+		var indexDesc []*IndexDesc
+		rows, err = m.conn.Query("show index from " + t.Name)
+		if err != nil {
+			return err
+		}
+		for rows.Next() {
+			id := new(IndexDesc)
+			rows.Scan(&id.Table, &id.Non_unique, &id.Key_name, &id.Seq_in_index, &id.Column_name, &id.Collation, &id.Cardinality,
+				&id.Sub_part, &id.Packed, &id.Null, &id.Index_type, &id.Comment, &id.Index_comment, &id.Visible, &id.Expression)
+			indexDesc = append(indexDesc, id)
 		}
 	}
 
