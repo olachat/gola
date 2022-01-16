baa85e7c5195c5a7296868101dce538ee925487d
问天 Weng Wei <weng.wei@olaola.chat>
2022-01-16 14:29:03 +0800

    Add gen test

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/dolthub/vitess v0.0.0-20211013185428-a8845fb919c1
 	github.com/friendsofgo/errors v0.9.2
 	github.com/go-sql-driver/mysql v1.6.0
+	github.com/google/go-cmp v0.3.0
 	github.com/spf13/viper v1.6.3
 	github.com/volatiletech/sqlboiler/v4 v4.6.0
 )
---
+++ main_test.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"embed"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/go-cmp/cmp"
+
+	sqle "github.com/dolthub/go-mysql-server"
+	"github.com/dolthub/go-mysql-server/auth"
+	"github.com/dolthub/go-mysql-server/memory"
+	"github.com/dolthub/go-mysql-server/server"
+	gsql "github.com/dolthub/go-mysql-server/sql"
+	"github.com/dolthub/go-mysql-server/sql/information_schema"
+	"github.com/olachat/gola/mysqldriver"
+	"github.com/volatiletech/sqlboiler/v4/drivers"
+)
+
+//go:embed testdata/*
+var fixtures embed.FS
+var s *server.Server
+var testDBPort int = 33066
+var testDBName string = "testdb"
+var testTables = []string{"users"}
+
+var update = flag.Bool("update", false, "update generated files")
+
+// init the database with tables based on .sql files in the testdb folder
+func init() {
+	engine := sqle.NewDefault(gsql.NewDatabaseProvider(
+		memory.NewDatabase(testDBName),
+		information_schema.NewInformationSchemaDatabase(),
+	))
+
+	config := server.Config{
+		Protocol: "tcp",
+		Address:  fmt.Sprintf("localhost:%d", testDBPort),
+		Auth:     auth.NewNativeSingle("root", "", auth.AllPermissions),
+	}
+	var err error
+
+	s, err = server.NewDefaultServer(config, engine)
+	if err != nil {
+		panic(err)
+	}
+
+	go s.Start()
+
+	connStr := mysqldriver.MySQLBuildQueryString("root", "", testDBName, "localhost", testDBPort, "false")
+	db, err := sql.Open("mysql", connStr)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, tableName := range testTables {
+		query, _ := fixtures.ReadFile("testdata/" + tableName + ".sql")
+
+		stmt, err := db.Prepare(string(query))
+		if err != nil {
+			panic(err.Error())
+		}
+
+		_, err = stmt.Exec()
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+type genMethod func(db *drivers.DBInfo, t drivers.Table) []byte
+
+func testGen(t *testing.T, wd string, gen genMethod, db *drivers.DBInfo, table drivers.Table, extName string) {
+	resultFile := gen(db, table)
+	if *update {
+		ioutil.WriteFile(wd+string(filepath.Separator)+"testdata"+string(filepath.Separator)+table.Name+"."+extName, resultFile, 0644)
+	} else {
+		expectedFile, _ := fixtures.ReadFile("testdata/" + table.Name + "." + extName)
+		if diff := cmp.Diff(resultFile, expectedFile); diff != "" {
+			t.Error("file different: ", table.Name+"."+extName)
+			fmt.Println(diff)
+		}
+	}
+}
+
+func TestCodeGen(t *testing.T) {
+	var config drivers.Config = map[string]interface{}{
+		"dbname":    testDBName,
+		"whitelist": testTables,
+		"host":      "localhost",
+		"port":      testDBPort,
+		"user":      "root",
+		"pass":      "",
+		"sslmode":   "false",
+	}
+
+	m := &mysqldriver.MySQLDriver{}
+	db, err := m.Assemble(config)
+	if err != nil {
+		panic(err)
+	}
+
+	wd, err := os.Getwd()
+	if err != nil {
+		wd = "."
+	}
+
+	for _, table := range db.Tables {
+		testGen(t, wd, genORM, db, table, "go")
+	}
+}
