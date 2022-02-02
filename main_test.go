ca9d8592dc7025088838abd3a5f353f946957132
问天 Weng Wei <weng.wei@olaola.chat>
2022-02-02 14:02:17 +0800

    Add blogs testdata

--- main_test.go
+++ main_test.go
@@ -29,7 +29,7 @@ var fixtures embed.FS
 var s *server.Server
 var testDBPort int = 33066
 var testDBName string = "testdb"
-var testTables = []string{"users"}
+var testTables = []string{"blogs", "users"}
 var testDataPath = "testdata" + string(filepath.Separator)
 
 var update = flag.Bool("update", false, "update generated files")
