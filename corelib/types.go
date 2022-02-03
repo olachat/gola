81496f19a27a36cf26677767d6bdc49955d1c6c7
问天 Weng Wei <weng.wei@olaola.chat>
2022-02-04 01:37:48 +0800

    Refine Ops

--- corelib/types.go
+++ corelib/types.go
@@ -8,10 +8,10 @@ type RowStruct interface {
 type Ops int
 
 const (
-	OpInit    Ops = 0
-	OpEqual   Ops = 1
-	OpIn      Ops = 2
-	OpGreater Ops = 3
-	OpSmaller Ops = 4
-	OpRange   Ops = 5
+	OpInit Ops = iota
+	OpEqual
+	OpIn
+	OpGreater
+	OpSmaller
+	OpRange
 )
