49204a28b982935a9df5dcaecc6623d476ec84b7
问天 Weng Wei <weng.wei@olaola.chat>
2022-02-03 18:10:29 +0800

    Add ops type

--- corelib/types.go
+++ corelib/types.go
@@ -8,3 +8,14 @@ type RowStruct interface {
 	GetColumnNames() string
 	GetPointers() []interface{}
 }
+
+type Ops int
+
+const (
+	OpInit    Ops = 0
+	OpEqual   Ops = 1
+	OpIn      Ops = 2
+	OpGreater Ops = 3
+	OpSmaller Ops = 4
+	OpRange   Ops = 5
+)
