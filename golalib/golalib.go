604ea91aba476e785e726948309f22121c49e69e
问天 Weng Wei <weng.wei@olaola.chat>
2022-07-25 15:10:12 +0800

    Improve panic msg

--- golalib/golalib.go
+++ golalib/golalib.go
@@ -75,7 +75,7 @@ func genTPL(t *structs.Table, tplName string) []byte {
 	t.VERSION = VERSION
 	err := ormtpl.GetTpl(tplName).Execute(buf, t)
 	if err != nil {
-		panic(err)
+		panic(tplName + " genTpl error:\n" + err.Error())
 	}
 	return buf.Bytes()
 }
